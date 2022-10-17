d05247e4ccdf5146d6703d0bafb7e43de653dea4
Fernando Caetano <nandoc@proconpb.local>
2022-10-17 13:23:48 -0300

    FINALIZADO

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -10,11 +10,13 @@ import Container from '../layout/Container'
 import ProjectForm from '../project/ProjectForm'
 import Message from '../layout/Message'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 
 function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
+    const [services, setServices] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
@@ -31,6 +33,7 @@ function Project() {
             .then((resp) => resp.json())
             .then((data) => {
                 setProject(data)
+                setServices(data.services)
             })
             .catch((err) => console.log(err))
         }, 400)
@@ -101,8 +104,38 @@ function Project() {
         .then((data) => {
             // exibir serviços
             console.log(data)
+            setShowServiceForm(false)
         })
         .catch(err => console.log(err))
+    }
+
+    function removeService( id, cost ){
+
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const projectUpdated = project
+
+        projectUpdated.services = servicesUpdated
+
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify(projectUpdated),
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+            setType('success')
+        })
+        .catch((err) => console.log(err))
     }
 
     function toggleProjectForm(){
@@ -113,6 +146,7 @@ function Project() {
         setShowServiceForm(!showServiceForm)
     }
 
+    const restante = project.budget - project.cost
 
     return ( 
         <>
@@ -135,8 +169,12 @@ function Project() {
                                 <span>Total Orçamento:</span> R${project.budget}
                             </p>
                             <p>
-                                <span>Total Utilizado:</span> R$ {project.cost}
+                                <span>Total Utilizado:</span> R${project.cost}
                             </p>
+                            <p>
+                                <span>Total disponível:</span> R${restante}
+                            </p>
+                            
                             </div>
                         ):(
                             <div className={styles.project_info}>
@@ -163,7 +201,23 @@ function Project() {
                     </div>
                     <h2>Serviços</h2>
                     <Container customClass="start">
-                            <p>Itens de serviços</p>
+                            {services.length > 0 &&
+                                services.map((service) => (
+                                    <ServiceCard
+                                        id={service.id}
+                                        name={service.name}
+                                        cost={service.cost}
+                                        description={service.description}
+                                        key={service.id}
+                                        handleRemove={removeService}
+
+                                    />
+                                ))
+
+                            }
+                            {services.length === 0 &&
+                                <p>Não há serviços cadastrados.</p>
+                            }
                     </Container>
                 </Container>
             </div>
